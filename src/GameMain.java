d47e7f6acb151ac474035a7c90ab53ea0d739cef
ASUS <nsodeifi78@gmail.com>
2021-01-14 22:23:05 +0330

    fixed bugs in sudoku board

--- src/GameMain.java
+++ src/GameMain.java
@@ -1,11 +1,10 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-import static java.lang.StrictMath.sqrt;
 
 
 public class GameMain {
-    private static ArrayList<Cell> sudoku = new ArrayList<>();
+    private static Sudoku sudoku;
 
     private static int get_Input() {
         char number;
@@ -16,32 +15,33 @@ public class GameMain {
         String[] colors = sc.nextLine().split(" ");
         Integer m = Integer.valueOf(input[0]);
         Integer n = Integer.valueOf(input[1]);
+        sudoku = new Sudoku(new Cell[n][n], n);
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 String val = sc.next();
                 color = val.charAt(1);
                 number = val.charAt(0);
                 Cell cell = new Cell(number, color);
-                sudoku.add(cell);
+                sudoku.getSudoku()[i][j] = cell;
             }
         }
         return n;
     }
 
     private static void print_Board(int n) {
-        for (int i = 0; i < sudoku.size(); i++) {
-            if (i % sqrt(sudoku.size()) == 0) {
-                System.out.print("\n");
-            }
+        for (int i = 0; i < sudoku.getSudoku().length; i++) {
+            System.out.print("\n");
+            for (int j = 0; j < sudoku.getSudoku().length; j++) {
 
-            if (sudoku.get(i) == null)
-                System.out.print("| ");
-            else if (sudoku.get(i) != null && (i + 1) % 3 == 0) {
-                System.out.print("|" + sudoku.get(i).getNumber() + sudoku.get(i).getColor() + "|");
-            } else {
-                System.out.print("|" + sudoku.get(i).getNumber() + sudoku.get(i).getColor());
-            }
+                if (sudoku.getSudoku()[i][j] == null)
+                    System.out.print("| ");
+                else if (sudoku.getSudoku()[i][j] != null && (j + 1) % 3 == 0) {
+                    System.out.print("|" + sudoku.getSudoku()[i][j].getNumber() + sudoku.getSudoku()[i][j].getColor() + "|");
+                }else {
+                    System.out.print("|" + sudoku.getSudoku()[i][j].getNumber() + sudoku.getSudoku()[i][j].getColor());
+                }
 
+            }
         }
     }
 
--- src/Sudoku.java
+++ src/Sudoku.java
@@ -1,2 +1,34 @@
 public class Sudoku {
+
+    public Cell[][] getSudoku() {
+        return sudoku;
+    }
+
+    private Cell[][] sudoku;
+    private int n ;
+
+    public Sudoku (Cell[][] sudoku , int n ){
+        this.sudoku = sudoku;
+        this.n = n;
+
+    }
+    public boolean CanPutNum(int i, int j, int x) {
+
+        // Is 'x' used in row.
+        for (int jj = 0; jj < n; jj++) {
+            if (sudoku[i][jj].getNumber() == 'x') {
+                return false;
+            }
+        }
+
+        // Is 'x' used in column.
+        for (int ii = 0; ii < n; ii++) {
+            if (sudoku[ii][j].getNumber() == 'x') {
+                return false;
+            }
+        }
+        // Everything looks good.
+        return true;
+    }
+
 }
